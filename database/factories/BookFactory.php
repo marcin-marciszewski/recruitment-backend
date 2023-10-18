047828a97b350c467a2b13c3a1096a55783c8613
Marcin Marciszewski <marciszewski.m@gmail.com>
2023-10-18 08:59:02 +0200

    Book factory oraz seeder.

---
+++ database/factories/BookFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Model>
+ */
+class BookFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'title' => $this->faker->sentence(),
+            'author' => $this->faker->name(),
+            'issue_year' => $this->faker->dateTime()->format('Y'),
+            'description' => $this->faker->paragraph(3),
+            'stock' => $this->faker->numberBetween(0, 100),
+            'category' => $this->faker->randomElement(['literatura', 'historia', 'nauka']),
+        ];
+    }
+}
